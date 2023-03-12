1c7f37bba08c0df21777cae022e0617848a32f26
maksymilianxx <126693634+maksymilianxx@users.noreply.github.com>
2023-03-13 00:13:41 +0100

    Delete WPRG_ZADANIA directory

--- WPRG_ZADANIA/1_1.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$rzut_kostka = rand(1,6); //losowanie wartosci od 1 do 6
-echo("Wynik rzutu kostka to: $rzut_kostka.");
-
-?>
--- WPRG_ZADANIA/1_2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$promien = 5; //wartosc promienia
-$srednica = $promien*2;
-echo("Średnica koła wynosi $srednica.");
-
-?>
